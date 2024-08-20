53ff2f329ad2fadd42e6aa2c92590f6f49ea54dc
Srinivasan <jpsrinivasan38@gmail.com>
2024-08-20 10:22:26 +0530

    MainPage edit

--- frontend/src/components/pages/MainPage.jsx
+++ frontend/src/components/pages/MainPage.jsx
@@ -4,59 +4,48 @@ import SideBar from '../SideBar'
 import Dashboard from '../fragments/Dashboard'
 import { doc, getDoc } from 'firebase/firestore'
 function MainPage( {setPage}) {
-    const [fragment,setFragment] = useState(["dashboard"]);
-    const [wideBar, setWideBar] = useState(false);
+    
     const [uID, setUID] = useState("");
     const [userData, setUserData] = useState({})
   
-    useEffect(()=>{
-    (async()=>{
-      try{
-      const docRef = doc(db, "user", uID);
-      const docSnap = await getDoc(docRef);
-      if(docSnap.exists()){
-        setUserData(docSnap.data());
-      }
-      else{
-        console.log("NO doc found");
-      }
-    }catch(err){
-      console.log("UID not loaded")
+    // useEffect(()=>{
+    // (async()=>{
+    //   try{
+    //   const docRef = doc(db, "user", uID);
+    //   const docSnap = await getDoc(docRef);
+    //   if(docSnap.exists()){
+    //     setUserData(docSnap.data());
+    //   }
+    //   else{
+    //     console.log("NO doc found");
+    //   }
+    // }catch(err){
+    //   console.log("UID not loaded")
+    // }
+    // })();
+    // },[uID]);
+    // useEffect(()=>{
+    //   auth.onAuthStateChanged((user) =>{
+    //     if(user){
+    //       setUID(user.uid)
+    //     }else{
+    //       console.log("Auth error")
+    //     }
+    //   })
+    // }, []);
+    const LogOut = () =>{
+      auth.signOut().then(()=>{ 
+        setPage("home")
+      }).catch((e)=>{
+    console.log(e.messgage);
+      });
     }
-    })();
-    },[uID]);
-    useEffect(()=>{
-      auth.onAuthStateChanged((user) =>{
-        if(user){
-          setUID(user.uid)
-        }else{
-          console.log("Auth error")
-        }
-      })
-    }, []);
-    const renderFragment = () => {
-        switch (fragment[0]) {
-          case "dashboard":
-            return <Dashboard wideBar={wideBar} setFragment={setFragment}/>;
-          case "profile":
-            return <Profile wideBar={wideBar} userData={userData} setWideBar={setWideBar} setPage={setPage}/>;
-          case "performance":
-            return <PerformanceST wideBar={wideBar} setFragment={setFragment}/>;
-          case "course":
-            return <Courses wideBar={wideBar} setFragment={setFragment}/>
-          case "courseView":
-            return <CourseViewPage wideBar={wideBar} setFragment={setFragment}
-            id={fragment[1]}/>
-          case "performanceTracker":
-            return <PerformanceTracker wideBar={wideBar} setFragment={setFragment}
-            id={fragment[1]} />
-          default:
-            return null;
-        }
-      };
+    
   return (
-    <div>
-        <Dashboard wideBar={wideBar} setFragment={setFragment}/>;
+    <div className='text-white flex justify-center items-center p-10'>
+      <button onClick={()=>LogOut()} className='bg-gray-500 px-10 text-2xl font-bold rounded-xl py-3 '>
+        Logout
+      </button>
     </div>
   )
 }
