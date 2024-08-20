63b26949b91702dafbb54ff6f32729817546fe7d
Srinivasan <jpsrinivasan38@gmail.com>
2024-08-20 10:50:25 +0530

    chore: home edit

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,16 +1,18 @@
-import React, { useState } from 'react'
-import Navigation from '../Navigation'
+import React from 'react';
+import Navigation from '../Navigation';
+import bg_main from "http://localhost:5173/bg_main.jpg";
 
-const Home = ({setPage}) => {
-
+const Home = ({ setPage }) => {
+  console.log(bg_main);
+  
   return (
-    <div id="home" className="mt-[66px]"
+    <div
+      id="home"
+      className="mt-[66px] bg-[url('../../assets/bg_main.jpg')]"
     >
-      <Navigation setPage={setPage}/>
-      {/*TODO: layout */}
+      <Navigation setPage={setPage} />
     </div>
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
