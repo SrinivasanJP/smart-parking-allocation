52c5106068aed51bb072b1d7ad812dfd5ff09425
Srinivasan <jpsrinivasan38@gmail.com>
2024-11-07 09:33:08 +0530

    fix: reserved data

--- frontend/src/components/SlotElement.jsx
+++ frontend/src/components/SlotElement.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const SlotElement = ({ data, handleSlotClick }) => (
     <div
-      className={`w-20 bg-gradient-to-bl h-36 flex justify-center items-center rounded-lg from-green-400/90 flex-col to-transparent cursor-pointer ${data.isReserved == 'RESERVED' && ' from-red-500 cursor-no-drop'}`}
+      className={`w-28 bg-gradient-to-bl h-36 flex justify-center items-center rounded-lg from-green-400/90 flex-col to-transparent cursor-pointer ${data.isReserved == 'RESERVED' && ' from-red-500 cursor-no-drop'}`}
       onClick={() => {
         handleSlotClick(data.id);
       }}
     >
       <h1>{data.id.toUpperCase()}</h1>
-      {data.isReserved && <p>Reserved</p>}
+      {<p>{data.isReserved}</p>}
     </div>
   );
 export default SlotElement
