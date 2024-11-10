458c69eeb45c09895d98b312cf081656e7a02fd0
Srinivasan <jpsrinivasan38@gmail.com>
2024-11-10 08:51:36 +0530

    fix: colors

--- frontend/src/components/SlotElement.jsx
+++ frontend/src/components/SlotElement.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SlotElement = ({ data, handleSlotClick }) => (
     <div
-      className={`w-28 bg-gradient-to-bl h-36 flex justify-center items-center rounded-lg from-green-400/90 flex-col to-transparent cursor-pointer ${data.isReserved == 'RESERVED' && ' from-red-500 cursor-no-drop'}`}
+      className={`w-28 bg-gradient-to-bl h-36 flex justify-center items-center rounded-lg flex-col to-transparent cursor-pointer ${data.isReserved == 'RESERVED'?'from-green-400/90':data.isReserved == "OCCUPIED"?'from-orange-400':' from-red-500 cursor-no-drop'}`}
       onClick={() => {
         handleSlotClick(data.id);
       }}
